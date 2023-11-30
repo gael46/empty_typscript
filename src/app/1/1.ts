32e6a31fddbb7d75c50770d4a5c1040d38fcaa73
gmarty <g@t.com>
2023-11-30 18:36:46 +0100

    feat: move input

--- src/app/1/1.ts
+++ src/app/1/1.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 export function main() {
-  const content = fs.readFileSync("src/input/1/1.txt", "utf8");
+  const content = fs.readFileSync("src/app/1/input.txt", "utf8");
 
   console.log("this is the main function that read  this content :", content);
 }
